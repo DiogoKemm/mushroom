4231cb8fb0c094dac3f0845ee86085e30a622544
Diogo Kemmerich <97892586+DiogoKemm@users.noreply.github.com>
2024-06-08 14:09:06 -0300

    Update mushroom.py

--- mushroom.py
+++ mushroom.py
@@ -1,14 +1,14 @@
 import pandas as pd
-import numpy as np
 import matplotlib.pyplot as plt
+import seaborn as sns
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import OneHotEncoder
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.metrics import accuracy_score, confusion_matrix, ConfusionMatrixDisplay
-from sklearn.tree import plot_tree
+from sklearn.tree import export_graphviz
 
 # Lê a tabela
-data = pd.read_csv('mushrooms.csv')
+data = pd.read_csv('mushroom/mushrooms.csv')
 data = data.drop(["veil-type"], axis=1)
 
 # OneHotEncoder
@@ -26,19 +26,24 @@ x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.3)
 
 # Árvore de decisão
 clf = DecisionTreeClassifier()
-clf = clf.fit(x_train, y_train)
-plt.figure(figsize=(12, 12))
-plot_tree(clf, feature_names=df_encoded.columns, fontsize=5)
-plt.show()
+clf.fit(x_train, y_train)
+dot_data = export_graphviz(clf, out_file='tree', feature_names=x.columns, filled=True, rounded=True, special_characters=True)
 
-# Previsão e acurácia - 0: comestível - 1: venenoso
+
+# Previsão e acurácia
 y_pred_dt = clf.predict(x_test)
-print(y_pred_dt)
 print("Accuracy: ", round(accuracy_score(y_test, y_pred_dt), 4), "%")
 
 # Matriz de confusão
 cm = confusion_matrix(y_test, y_pred_dt)
-disp = ConfusionMatrixDisplay(confusion_matrix=cm)
-disp.plot()
-disp.figure_.savefig('confusion_matrix.png')
+x_axis_labels = ["Comestível", "Venenoso"]
+y_axis_labels = ["Comestível", "Venenoso"]
+f, ax = plt.subplots(figsize =(7,7))
+sns.heatmap(cm, annot = True, linewidths=0.2, linecolor="black", fmt = ".0f", ax=ax, cmap="Purples", xticklabels=x_axis_labels, yticklabels=y_axis_labels)
+plt.xlabel("PREDICTED LABEL")
+plt.ylabel("TRUE LABEL")
+plt.title('Matriz de confusão para árvore de decisão')
+plt.savefig("dtcm.png", format='png', dpi=500, bbox_inches='tight')
+#plt.show()
+
 
